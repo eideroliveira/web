ec89b08f849ab13ac7b70bd94cdddeda9b622012
Eider Oliveira <eider.oliveira@gmail.com>
2025-12-21 18:29:33 -0300

    Please provide the code changes (diff) for me to summarize.

--- api.go
+++ api.go
@@ -122,12 +122,17 @@ func (ctx *EventContext) MustUnmarshalForm(v interface{}) {
 
 func (ctx *EventContext) UnmarshalForm(v interface{}) (err error) {
 	mf := ctx.R.MultipartForm
-	if ctx.R.MultipartForm == nil {
+	var values map[string][]string
+	if mf != nil {
+		values = mf.Value
+	} else if ctx.R.Form != nil {
+		values = ctx.R.Form
+	} else {
 		return
 	}
 
 	dec := form.NewDecoder()
-	err = dec.Decode(v, mf.Value)
+	err = dec.Decode(v, values)
 	if err != nil {
 		// panic(err)
 		return
